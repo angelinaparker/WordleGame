75a86e27fd5148866da9e2f1da5a75d5c9beb001
nuna <nuna@nunas-MacBook-Pro.local>
2022-09-06 14:47:09 -0700

    adjusted keyboard

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -47,12 +47,12 @@ function Keyboard() {
         {keys1.map((key) => {
           return <Key keyVal={key} disabled = {disabledLetters.includes(key)} />;
         })}
-      </div>
+      </div> <br></br>
       <div className="line2">
         {keys2.map((key) => {
           return <Key keyVal={key} disabled = {disabledLetters.includes(key)} />;
         })}
-      </div>
+      </div> <br></br>
       <div className="line3">
         <Key keyVal={"ENTER"} bigKey />
         {keys3.map((key) => {
